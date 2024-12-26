0f2dc82df428137ec8aea3738dedf275ae9061e7
Sushma_Srivalli_Gayathri_Anaparthi <sushma.svgayathri16@gmail.com>
2024-12-26 10:46:17 +0530

    Initial commit

---
+++ login-signup-app/src/components/Signout.js
@@ -0,0 +1,14 @@
+function Signout({ setActiveContent }) {
+    return (
+      <div>
+        <ul>
+          <li onClick={() => setActiveContent("signout")}>
+            <a href="/">Sign Out</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Signout;
+  
